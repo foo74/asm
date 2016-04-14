1b65e70924dabf77cbee84b111bd9fc54a61f4e8
Foo174 <withee@gmail.com>
2016-04-13 20:51:05 -0700

    tweaking code

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,4 +1,6 @@
-#include <screen.h>
+#include "screen.h"
+
+/*
 void print_char(char character, int col, int row, char attribute_byte)
 {
    unsigned char *vidmem = (unsigned char *)VIDEO_ADDRESS;
@@ -25,4 +27,63 @@ void print_char(char character, int col, int row, char attribute_byte)
 
    offset = handle_scrolling(offset);
    set_cursor(offset);
+}
+*/
+
+void set_cursor()
+{
+   port_byte_out(REG_SCREEN_CTRL, 14);
+   port_byte_out(REG_SCREEN_DATA, 0x00);
+   port_byte_out(REG_SCREEN_CTRL, 15);
+   port_byte_out(REG_SCREEN_DATA, 0x50);
+}
+/*****************************************************************************
+ * A C wrapper function that reads a byte from the specified port
+ * "=a" (result) means: put AL register in variable RESULT when finished
+ * "d" (port) means: load EDX with port.
+ *****************************************************************************/
+unsigned char port_byte_in(unsigned short port)
+{
+   unsigned char result;
+   __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+   return result;
+}
+
+void port_byte_out(unsigned short port, unsigned char data)
+{
+   __asm__("out %%al, %%dx" : :"a" (data), "d" (port));
+}
+
+unsigned short port_word_in(unsigned short port)
+{
+   unsigned short result;
+   __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
+   return result;
+}
+
+void port_word_out(unsigned short port, unsigned short data)
+{
+   __asm__("out %%ax, %%dx" : :"a" (data), "d" (port));
+}
+void print_message(char* video_memory, char s[])
+{
+   int i;
+   for (i=0; s[i] != 0; i++)
+   {
+      *video_memory++ = s[i];
+      *video_memory++ = CHAR_COLOR;
+   }
+}
+
+void clear_screen(char* video_memory)
+{
+   int i, j;
+   for (i=0; i<25; i++)
+   {
+      for (j=0; j<80; j++)
+      {
+         *video_memory++ = ' ';
+         *video_memory++ = CHAR_COLOR;
+      }
+   }
 }
--- drivers/screen.h
+++ drivers/screen.h
@@ -1,10 +1,35 @@
-#define VIDEO_ADDRESS 0xb8000
-#define MAX_ROWS 25
-#define MAX_COLS 80
+#include <stdio.h>
+/* #include <stdint.h> so we can cast int to pointer type on line 19. */
+
+/**************************************************************************
+ * Attribute byte code. first 4 bits are background, next 4 are foreground.
+ * 0 = BLACK            8 = DARK GREY
+ * 1 = BLUE             9 = LIGHT BLUE
+ * 2 = GREEN           10 = LIGHT GREEN
+ * 3 = CYAN            11 = LIGHT CYAN
+ * 4 = RED             12 = LIGHT RED
+ * 5 = MAGENTA         13 = LIGHT MAGENTA
+ * 6 = BROWN           14 = LIGHT BROWN
+ * 7 = LIGHT GREY      15 = WHITE
+ **************************************************************************/
 
 /* Attribute byte for our default color scheme. */
 #define GREEN_ON_BLACK 0x02
-
+#define CHAR_COLOR 0x02
+#define VIDEO_MEMORY 0xb8000
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_COLS 80
+#define MAX_ROWS 25
 /* Screen device I/O ports. */
 #define REG_SCREEN_CTRL 0x3D4
 #define REG_SCREEN_DATA 0x3D5
+
+
+void set_cursor();
+void print_char(char character, int col, int row, char attribute_byte);
+unsigned char port_byte_in(unsigned short port);
+void port_byte_out(unsigned short port, unsigned char data);
+unsigned short port_word_in(unsigned short port);
+void port_word_out(unsigned short port, unsigned short data);
+void clear_screen(char *);
+void print_message(char* video_memory, char s[]);
